37a459bdd77bad237582c7f1d8bcb926140e3414
wzw <noingw96@163.com>
2019-11-06 16:43:53 +0800

    药材分布创建

--- static/js/class.js
+++ static/js/class.js
@@ -1,4 +1,9 @@
 (function () {
+    var allData=[];
+    var len = 0;
+    var pageNum = 1;
+    var perNum = 8;
+    //请求分类信息
     $(".category").on("click",function(){
         var drugId = $(this).attr("data-id");
         var drugName = $(this).text();
@@ -6,7 +11,10 @@
             drugId:drugId,
             drugName:drugName
         }
-        var allData=[]
+        allData=[];
+        len = 0;
+        pageNum = 1;
+        perNum = 8;
         $.ajax({
             type:'get',
             url:'/requestClass',
@@ -14,19 +22,70 @@
             headers:{"Content-Type":"application/json"},
             dataType:'json',
             success:function(ret){
-                var allData=ret.elements.content;
+                allData=ret.elements.content;
+                len = allData.length;
                 console.log(ret);
                 var li="";
-                $.each(allData.slice(0,8),function (i,val) {
+                $.each(allData.slice(0,pageNum*perNum),function (i,val) {
                     li+='<li>' +
-                        '<div class="drug-item" data-id="' + val.drugId + '">' +
-                        '<img class="drug-item-img" src="../static/images/drugItem.jpg" alt="">' +
-                        '<div class="drug-item-name">' + val.drugName + '</div>' +
+                        '<div class="drug-item" data-id="' + val.drugId + '">';
+                    if(val.img == "无"||val.img.indexOf("nopic")>-1){
+                        li+='<img class="drug-item-img" src="../static/images/drugItem.jpg" alt="">';
+                    }else {
+                        li+='<img class="drug-item-img" src="'+val.img+'" alt="">';
+                    }
+                    li+='<div class="drug-item-name">' + val.drugName + '</div>' +
                         '</div>' +
                         '</li>'
                 })
                 $(".drug-list").html(li);
+                if(len >= pageNum*perNum){
+                    $(".load-more").show();
+                }
             }
         })
+    })
+    //加载更多
+    $(".load-more").on('click',function () {
+        var li="";
+        $.each(allData.slice(pageNum*perNum,(pageNum+1)*perNum),function (i,val) {
+            li+='<li>' +
+                '<div class="drug-item" data-id="' + val.drugId + '">' +
+                '<img class="drug-item-img" src="../static/images/drugItem.jpg" alt="">' +
+                '<div class="drug-item-name">' + val.drugName + '</div>' +
+                '</div>' +
+                '</li>'
+        })
+        $(".drug-list").append(li);
+        pageNum++;
+        if(len >= pageNum*perNum){
+            $(".load-more").show();
+        }else {
+            $(".load-more").hide();
+        }
+    })
+    //关闭
+    $(".close").on('click',function () {
+        $(".detail-page").hide();
+    })
+    //点击详情
+    $(document).on('click','.drug-item',function (e) {
+        var curDrugId = $(this).attr("data-id");
+        var curObj = {};
+        for(var i=0;i<allData.length;i++){
+            if(allData[i].drugId == curDrugId ){
+                curObj = allData[i];
+                break;
+            }
+        }
+        $("#name").text(curObj.drugName);
+        $("#othername").text(curObj.otherName);
+        $("#newtime").text(curObj.newTime);
+        $("#position").text(curObj.productionArea);
+        $("#spec").text(curObj.characteristic);
+        if(curObj.img!="无"){
+            $("#myImg").attr('src',curObj.img);
+        }
+        $(".detail-page").show();
     })
 })()
